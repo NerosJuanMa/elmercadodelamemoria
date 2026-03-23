51ce4d0c35070725602672108e4e0354892429c5
Neros__JuanMa <jmmudarra@gmail.com>
2026-03-23 11:13:15 +0100

    front

---
+++ frontend/src/components/card.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Card.css';
+
+const Card = ({ nombre, descripcion, precio, imagen }) => {
+ flex-flow: column;
+    justify-self: center;
+    align-items: center; 
+    gap: 1px; 
+    color: #4B3621; 
+    border: 1px solid #C8A96A;
+    border-radius: 10px;
+    position: fixed;
+    top: 1.5%;
+    width: 15%;
+    backdrop-filter: blur(10px);
+  );
+};
+
+export default Card;
--- frontend/src/pages/Items.jsx
+++ frontend/src/pages/Items.jsx
@@ -1,12 +1,26 @@
 // pages/Items.jsx
 import './Items.css';
 export default function Items() {
+  const productoEjemplo = {
+    nombre: "Reloj de Bolsillo Antiguo",
+    descripcion: "Reloj de plata del siglo XIX, funciona perfectamente.",
+    precio: "120.50",
+    imagen: "https://via.placeholder.com/150"
+  };
   return (
     <>
       <section className="title"><h2>🎁 Productos</h2></section>
       <section className='filtro_categoria'>Filtrar categoría</section>
       <section className="card_item">            
         <section className="card_producto">
+          <section className="catalogo">
+      <Card 
+        nombre={productoEjemplo.nombre}
+        descripcion={productoEjemplo.descripcion}
+        precio={productoEjemplo.precio}
+        imagen={productoEjemplo.imagen}
+      />
+    </section>
             <section className="producto">
                 <p>Producto1</p>
             </section>
